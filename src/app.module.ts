1587d16d660b95dd46126de72b6a8b1a1ff4a9b7
Enderson Vizcáino <enjaviga@gmail.com>
2020-10-12 04:51:10 +0000

    feat(graphql): configurando GraphQl en app

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { routes } from './routes';
 import { V1Module } from './v1/v1.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { join } from 'path';
+import { GraphQLModule } from '@nestjs/graphql';
 
 @Module({
   imports: [
@@ -12,11 +13,19 @@ import { join } from 'path';
     TypeOrmModule.forRoot({
       type: 'mongodb',
       url:
-        'mongodb://root:root@mongo:27017/?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=false',
+        'mongodb://root:root@mongo:27017/fakedata_db?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=false',
       entities: [join(__dirname, '**/**.entity{.js,.ts}')],
       synchronize: true,
       useNewUrlParser: true,
       logging: true,
+    }),
+    GraphQLModule.forRoot({
+      // Caracteristica para trabajar con "Code first"
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
+      buildSchemaOptions: {
+        dateScalarMode: 'timestamp',
+      },
     }),
   ],
 })
