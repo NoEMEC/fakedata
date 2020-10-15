b0b66331c561c8ed1454d7471c89b54975a33aec
JoralmoPro <JoralmoPro@gmail.com>
2020-10-14 22:54:37 -0500

    refactor(config): using for typrOrm configuration and port for server listening 🎨

---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,14 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import * as config from 'config';
+
+const dbConfig = config.get('db');
+
+export const typeOrmConfig: TypeOrmModuleOptions = {
+    type: dbConfig.type,
+    url: process.env.DB_URL || dbConfig.url,
+    port: process.env.DB_PORT || dbConfig.port,
+    entities: [__dirname + '/../**/**.entity{.js,.ts}'],
+    useNewUrlParser: true,
+    logging: true,
+    synchronize: dbConfig.synchronize,
+};
