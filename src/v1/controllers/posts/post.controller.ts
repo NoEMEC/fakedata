f57e41e3d993e2006c78476d52588cd8deda6642
JoralmoPro <JoralmoPro@gmail.com>
2020-10-15 00:28:20 -0500

    feat(post): inject and use post service ✨

--- src/v1/controllers/posts/post.controller.ts
+++ src/v1/controllers/posts/post.controller.ts
@@ -1,9 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
+import { PostEntity } from './post.entity';
+import { PostService } from './post.service';
 
 @Controller()
 export class PostsController {
+    constructor(private postService: PostService) {}
+
     @Get()
-    posts(): string {
-        return 'All Post :D';
+    getPosts(): Promise<PostEntity[]> {
+        return this.postService.getPosts();
     }
 }
