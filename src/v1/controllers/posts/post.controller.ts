fb4e955cf8ad2021e1f207853567e429a71ba45e
JoralmoPro <JoralmoPro@gmail.com>
2020-10-15 22:12:09 -0500

    feat(post): crud posts in controller ✨

--- src/v1/controllers/posts/post.controller.ts
+++ src/v1/controllers/posts/post.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+    Body,
+    Controller,
+    Delete,
+    Get,
+    Param,
+    Post,
+    ValidationPipe,
+} from '@nestjs/common';
+import { PostDTO } from './dto/post.dto';
 import { PostEntity } from './post.entity';
 import { PostService } from './post.service';
 
@@ -9,5 +18,20 @@ export class PostsController {
     @Get()
     getPosts(): Promise<PostEntity[]> {
         return this.postService.getPosts();
+    }
+
+    @Get(':id')
+    getPost(@Param('id') id: string): Promise<PostEntity> {
+        return this.postService.getPost(id);
+    }
+
+    @Post()
+    createPost(@Body(ValidationPipe) post: PostDTO): Promise<PostEntity> {
+        return this.postService.createPost(post);
+    }
+
+    @Delete(':id')
+    deletePost(@Param('id') id: string): Promise<string> {
+        return this.postService.deletePost(id);
     }
 }
