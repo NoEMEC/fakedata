d64ac5e5c328fa34da3bc7637b23eef989be152c
Enderson Vizcáino <enjaviga@gmail.com>
2020-10-16 03:57:36 +0000

    feat(graphql): defined UserDTO type for graphql

--- src/v1/controllers/users/dto/user.dto.ts
+++ src/v1/controllers/users/dto/user.dto.ts
@@ -4,25 +4,25 @@ import { InputType, Field } from '@nestjs/graphql';
 
 @InputType()
 export class UserDTO {
-  @Field()
-  @IsNotEmpty()
-  firstname: string;
+    @Field()
+    @IsNotEmpty()
+    firstname: string;
 
-  @Field()
-  @IsNotEmpty()
-  lastname: string;
+    @Field()
+    @IsNotEmpty()
+    lastname: string;
 
-  @Field()
-  @IsNotEmpty()
-  age: number;
+    @Field()
+    @IsNotEmpty()
+    age: number;
 
-  @Field()
-  @IsNotEmpty()
-  @IsDateString()
-  birthdate: string;
+    @Field()
+    @IsNotEmpty()
+    @IsDateString()
+    birthdate: string;
 
-  @Field()
-  @IsNotEmpty()
-  @IsIn([UserGender.FEMALE, UserGender.MALE, UserGender.OTHER])
-  gender: string;
+    @Field()
+    @IsNotEmpty()
+    @IsIn([UserGender.FEMALE, UserGender.MALE, UserGender.OTHER])
+    gender: string;
 }
