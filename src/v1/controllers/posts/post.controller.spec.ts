7a9add17eba011281e962dd9c87748741a167fa5
JoralmoPro <JoralmoPro@gmail.com>
2020-10-16 16:04:02 -0500

    feat(post-controller): testing e2e controller post ✅

--- src/v1/controllers/posts/post.controller.spec.ts
+++ src/v1/controllers/posts/post.controller.spec.ts
@@ -1,18 +1,77 @@
+import { INestApplication } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
-import { PostsController } from './post.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import { PostsModule } from './post.module';
+import { PostRepository } from './post.repository';
+import request from 'supertest';
+import { PostEntity } from './post.entity';
 
-describe('PostsController', () => {
-    let controller: PostsController;
+const mongoMemory = new MongoMemoryServer();
 
+const testPost = {
+    title: 'Test title',
+    body: 'Test body',
+    userId: 'randomId',
+};
+
+let randomId;
+
+describe('PostsController & e2e', () => {
+    let app: INestApplication;
     beforeEach(async () => {
+        const url = await mongoMemory.getUri();
         const module: TestingModule = await Test.createTestingModule({
-            controllers: [PostsController],
+            imports: [
+                PostsModule,
+                TypeOrmModule.forRoot({
+                    type: 'mongodb',
+                    url,
+                    entities: [PostEntity],
+                    keepConnectionAlive: true,
+                    useUnifiedTopology: true,
+                    useNewUrlParser: true,
+                }),
+                TypeOrmModule.forFeature([PostRepository]),
+            ],
         }).compile();
 
-        controller = module.get<PostsController>(PostsController);
+        app = module.createNestApplication();
+        await app.init();
+    });
+
+    afterAll(async () => {
+        await app.close();
     });
 
     it('should be defined', () => {
-        expect(controller).toBeDefined();
+        expect(true).toBe(true);
+    });
+
+    it(`/GET posts`, async () => {
+        return request(app.getHttpServer())
+            .get('/')
+            .expect(200)
+            .expect([]);
+    });
+
+    it('/POST posts', async done => {
+        return request(app.getHttpServer())
+            .post('/')
+            .send(testPost)
+            .expect(201)
+            .then(res => {
+                const { _id, ...postWithoutId } = res.body;
+                randomId = _id;
+                expect(postWithoutId).toEqual(testPost);
+                done();
+            });
+    });
+
+    it(`/GET post`, async () => {
+        return request(app.getHttpServer())
+            .get(`/${randomId}`)
+            .expect(200)
+            .expect({ ...testPost, _id: randomId });
     });
 });
