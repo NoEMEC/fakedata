8f39e8aa2f184b2b6dfd4111effd192eba400fc6
JoralmoPro <JoralmoPro@gmail.com>
2020-10-15 22:13:26 -0500

    feat(post): simulate create post ✨

--- src/v1/controllers/posts/post.repository.ts
+++ src/v1/controllers/posts/post.repository.ts
@@ -1,5 +1,24 @@
 import { EntityRepository, Repository } from 'typeorm';
+import { PostDTO } from './dto/post.dto';
 import { PostEntity } from './post.entity';
+import { ObjectId } from 'mongodb';
+import { InternalServerErrorException } from '@nestjs/common';
 
 @EntityRepository(PostEntity)
-export class PostRepository extends Repository<PostEntity> {}
+export class PostRepository extends Repository<PostEntity> {
+    async createPost(post: PostDTO): Promise<PostEntity> {
+        // eslint-disable-next-line @typescript-eslint/camelcase
+        const { title, body, user_id } = post;
+        const postEntity = new PostEntity();
+        postEntity.title = title;
+        postEntity.body = body;
+        // eslint-disable-next-line @typescript-eslint/camelcase
+        postEntity.user_id = user_id;
+        postEntity._id = new ObjectId().toHexString();
+        try {
+            return postEntity;
+        } catch (error) {
+            throw new InternalServerErrorException();
+        }
+    }
+}
