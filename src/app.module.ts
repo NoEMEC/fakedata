45f1b4ef498363507d2bbf0fcfd2f336b561da09
Enderson Vizcáino <enjaviga@gmail.com>
2020-10-16 03:54:00 +0000

    feat(graphql): config GraphQLModule :ok_hand:

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,17 +8,18 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { typeOrmConfig } from './config/typeorm.config';
 
 @Module({
-  imports: [
-    RouterModule.forRoutes(routes),
-    V1Module,
-    TypeOrmModule.forRoot(typeOrmConfig),
-    GraphQLModule.forRoot({
-      // Caracteristica para trabajar con "Code first"
-      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
-      sortSchema: true,
-      buildSchemaOptions: {
-        dateScalarMode: 'timestamp',
-      },
-    }),
-  ]})
+    imports: [
+        RouterModule.forRoutes(routes),
+        V1Module,
+        TypeOrmModule.forRoot(typeOrmConfig),
+        GraphQLModule.forRoot({
+            // Caracteristica para trabajar con "Code first"
+            autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+            sortSchema: true,
+            buildSchemaOptions: {
+                dateScalarMode: 'timestamp',
+            },
+        }),
+    ],
+})
 export class AppModule {}
