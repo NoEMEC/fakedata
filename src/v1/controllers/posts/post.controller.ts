38f45a709add887a38867a1315a2e1105a98a883
JoralmoPro <JoralmoPro@gmail.com>
2020-11-15 20:26:58 -0500

    feat(doc): adding decorators for swagger doc 📝️

--- src/v1/controllers/posts/post.controller.ts
+++ src/v1/controllers/posts/post.controller.ts
@@ -3,34 +3,47 @@ import {
     Controller,
     Delete,
     Get,
+    HttpCode,
     Param,
     Post,
     ValidationPipe,
 } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { Pagination } from 'src/paginate';
 import { PostDTO } from './dto/post.dto';
 import { PostEntity } from './post.entity';
 import { PostService } from './post.service';
 
+@ApiTags('posts')
 @Controller()
 export class PostsController {
     constructor(private postService: PostService) {}
 
     @Get()
+    @ApiOperation({ summary: 'Get all post by pages' })
+    @ApiResponse({ status: 200, type: Pagination })
     getPosts(): Promise<PostEntity[]> {
         return this.postService.getPosts();
     }
 
     @Get(':id')
+    @ApiOperation({ summary: 'Get post by id' })
+    @ApiResponse({ status: 200, type: PostEntity })
     getPost(@Param('id') id: string): Promise<PostEntity> {
         return this.postService.getPost(id);
     }
 
     @Post()
+    @ApiOperation({ summary: 'Create post' })
+    @ApiResponse({ status: 201, type: PostEntity })
     createPost(@Body(ValidationPipe) post: PostDTO): Promise<PostEntity> {
         return this.postService.createPost(post);
     }
 
     @Delete(':id')
+    @HttpCode(204)
+    @ApiOperation({ summary: 'Delete user by id' })
+    @ApiResponse({ status: 204, type: String })
     deletePost(@Param('id') id: string): Promise<string> {
         return this.postService.deletePost(id);
     }
--- src/v1/controllers/user/user.controller.ts
+++ src/v1/controllers/user/user.controller.ts
@@ -10,20 +10,24 @@ import {
     HttpCode,
     Query,
 } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { UserService } from './user.service';
 import { UserDTO } from './dto/user.dto';
 import { User } from './user.entity';
-import { Pagination, PaginationOptionsInterface } from 'src/paginate';
+import { Pagination, PaginationOptionsDTO } from 'src/paginate';
 
+@ApiTags('users')
 @Controller()
 export class UserController {
     constructor(private userService: UserService) {}
 
     @Get()
+    @ApiOperation({ summary: 'Get all users by pages' })
+    @ApiResponse({ status: 200, type: Pagination })
     getAllUsers(
-        @Query() options: PaginationOptionsInterface,
+        @Query() options: PaginationOptionsDTO,
     ): Promise<Pagination<User>> {
-        const optionsPaginate: PaginationOptionsInterface = {
+        const optionsPaginate: PaginationOptionsDTO = {
             page: Number(options.page) || 1,
             limit: Number(options.limit) || 10,
         };
@@ -31,22 +35,30 @@ export class UserController {
     }
 
     @Get(':id')
+    @ApiOperation({ summary: 'Get user by id' })
+    @ApiResponse({ status: 200, type: User })
     getUser(@Param('id') id: string): Promise<User> {
         return this.userService.getUser(id);
     }
 
     @Post()
+    @ApiOperation({ summary: 'Create user' })
+    @ApiResponse({ status: 201, type: User })
     createUser(@Body(ValidationPipe) user: UserDTO): Promise<User> {
         return this.userService.createUser(user);
     }
 
     @Patch(':id')
+    @ApiOperation({ summary: 'Update user by id' })
+    @ApiResponse({ status: 200, type: User })
     updateUser(@Param('id') id: string, @Body('user') user: Partial<UserDTO>) {
         return this.userService.updateUser(id, user);
     }
 
     @Delete(':id')
     @HttpCode(204)
+    @ApiOperation({ summary: 'Delete user by id' })
+    @ApiResponse({ status: 204, type: String })
     deleteUser(@Param('id') id: string): Promise<string> {
         return this.userService.deleteUser(id);
     }
