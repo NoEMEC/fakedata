5733a89d4802428beafc89e791e0a1c75738063a
JoralmoPro <JoralmoPro@gmail.com>
2020-11-15 20:27:50 -0500

    feat(doc): adding decorators for swagger doc 📝️

--- src/v1/controllers/posts/dto/post.dto.ts
+++ src/v1/controllers/posts/dto/post.dto.ts
@@ -1,12 +1,16 @@
 import { IsNotEmpty } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class PostDTO {
+    @ApiProperty()
     @IsNotEmpty()
     title: string;
 
+    @ApiProperty()
     @IsNotEmpty()
     body: string;
 
+    @ApiProperty()
     @IsNotEmpty()
     userId: string;
 }
--- src/v1/controllers/user/dto/user.dto.ts
+++ src/v1/controllers/user/dto/user.dto.ts
@@ -1,26 +1,32 @@
 import { IsNotEmpty, IsIn, IsDateString } from 'class-validator';
 import { UserGender } from '../user-gender';
 import { InputType, Field } from '@nestjs/graphql';
+import { ApiProperty } from '@nestjs/swagger';
 
 @InputType()
 export class UserDTO {
+    @ApiProperty()
     @Field()
     @IsNotEmpty()
     firstname: string;
 
+    @ApiProperty()
     @Field()
     @IsNotEmpty()
     lastname: string;
 
+    @ApiProperty()
     @Field()
     @IsNotEmpty()
     age: number;
 
+    @ApiProperty({ default: new Date().toISOString() })
     @Field()
     @IsNotEmpty()
     @IsDateString()
     birthdate: string;
 
+    @ApiProperty({ default: UserGender.OTHER })
     @Field()
     @IsNotEmpty()
     @IsIn([UserGender.FEMALE, UserGender.MALE, UserGender.OTHER])
